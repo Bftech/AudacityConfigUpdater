886cf3bb977906d42ab5850a2c0727e44baf896c
Unknown <corentin.monmasson@gmail.com>
2019-02-13 17:16:36 +0100

    cfg changes

--- main.py
+++ main.py
@@ -3,29 +3,27 @@ import requests
 import logging
 import os, sys
 
-config = configparser.ConfigParser()
-
 remote_config_url = "https://raw.githubusercontent.com/Bftech/AudacityConfigUpdater/dev/remote_configs/ANA.cfg"
 
 platform = sys.platform
 if platform == 'win32' or platform == 'cygwin':
     local_config_path = os.getenv('APPDATA') + "\\audacity\\audacity.cfg"  # TODO : A verifier
 elif platform == 'linux':
-    local_config_path = ""
+    local_config_path = "~/.audacity-data/audacity.cfg"
 
 
-try:
-    logging.log(logging.INFO, "Fetching remote cfg...")
-    remote_config = requests.get(remote_config_url).text
-    
-    config.read_string(remote_config)
+def writeConfig():
+    try:
+        config = configparser.ConfigParser()
+        logging.log(logging.INFO, "Fetching remote cfg...")
+        remote_config = requests.get(remote_config_url).text
+        
+        config.read_string(remote_config)
 
-    with open(local_config_path, 'w') as configfile:
-        config.write(configfile)
-        logging.log(logging.info, "Local cfg updated")
+        with open(local_config_path, 'w') as configfile:
+            config.write(configfile)
+            logging.log(logging.info, "Local cfg updated")
 
-except requests.exceptions.RequestException as e:
-    # NO INTERNET !
-    logging.log(logging.ERROR, "No remote cfg")
-    pass 
-
+    except requests.exceptions.RequestException as e:
+        # NO INTERNET !
+        logging.log(logging.ERROR, "No remote cfg")
